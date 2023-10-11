3ba49a1d0a0d8a85e1cebbcbf67db237068557cb
Bartosz Nowak <bartekn337@gmail.com>
2023-10-11 12:47:24 +0200

    Suppress the flurry of warnings caused by using C naming conventions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Suppress the flurry of warnings caused by using "C" naming conventions
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 use std::ops::Shl;
 
 use crate::bindings::{
