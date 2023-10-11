3b6d234325f9537fdce2c02137e88d0a97b0bf5c
Bartosz Nowak <bartekn337@gmail.com>
2023-10-11 10:30:24 +0200

    binding working&error messaging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::bindings::examples::simple_create_and_verify_proof;
+
 pub mod bindings;
 
 // This matches bindgen::Builder output
@@ -5,4 +7,5 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 fn main() {
     println!("Hello, world!");
+    println!("{}", simple_create_and_verify_proof().unwrap());
 }
