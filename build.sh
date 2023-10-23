a176a4f9aaf7dc1852ec7e2f46e8b619a31984f4
Bartosz Nowak <bartoszn337@gmail.com>
2023-10-23 10:46:01 +0200

    readme update

--- build.sh
+++
@@ -1,8 +0,0 @@
-cd barretenberg/aztec-packages/barretenberg/cpp/
-# Clean.
-rm -rf ./build
-rm -rf ./build-wasm
-
-PRESET=default
-cmake --preset $PRESET -DCMAKE_BUILD_TYPE=RelWithAssert
-cmake --build --preset $PRESET ${@/#/--target }
